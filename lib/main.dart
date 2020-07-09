1707522adfdb4a1a7b3302b1c52da303e10a119c
101Ben <tbenaja@outlook.fr>
2020-07-09 11:19:46 -0700

    Content commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Youth and Law Forum',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
